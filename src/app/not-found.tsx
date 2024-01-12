e8aa6cf7fbe4e486928f1e8f6f1380da803e4727
scarletvargas <scarletvf47@gmail.com>
2024-01-11 22:26:16 -0400

    Page 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Button } from "app/components/shared/Button";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center gap-4 flex-col h-full">
+      <h2 className="text-border text-6xl font-semibold">404</h2>
+      <p className="text-text">This page doesn't exist.</p>
+      <Button variant="white" type="link" href="/">
+        Return Home
+      </Button>
+    </div>
+  );
+}
