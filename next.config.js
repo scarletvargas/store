d91507f685f16455e23b7c526b940019c60ef161
scarletvargas <scarletvf47@gmail.com>
2024-01-12 22:57:17 -0400

    updating next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const path = require('path')
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'cdn.shopify.com',
+        protocol: 'https',
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
