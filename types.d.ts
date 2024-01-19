ee6fa1742904b4c36c81f8193ad1b8380af8054d
scarletvargas <scarletvf47@gmail.com>
2024-01-19 18:51:58 -0400

    file changes

--- types.d.ts
+++ types.d.ts
@@ -4,3 +4,30 @@ interface PropsIcons {
   height?: number;
   strokeWidth?: number;
 }
+
+type ProductType = {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  quantity: number;
+  handle: string;
+  tags: string;
+  gql_id: string;
+};
+
+type CollectionType = {
+  id: string;
+  title: string;
+  handle: string;
+}
+
+type CartItem = {
+  title: string;
+  price: number;
+  quantity: number;
+  id: string;
+  image: string;
+  merchandiseId: string;
+}
